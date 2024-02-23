67a598abb6d29ba5d0f54177eb607b1843bf6c81
Aran <aranmcc7@outlook.com>
2024-02-23 09:50:47 +0000

    diary entry markup and styling

---
+++ frontend/src/diaryEntry.js
@@ -0,0 +1,45 @@
+import "./diaryEntry.css"
+
+function DiaryEntry() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //Submit code
+  };
+
+  return (
+<div class="container">
+  <form>
+  <div class="row">
+    <div class="col-25">
+      <label for="sname">Spot Name</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="sname" name="sname" placeholder="Spot..."/>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="coords">Coordinates</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="coords" name="coordinates" placeholder="Coordinates..."/>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="notes">Notes</label>
+    </div>
+    <div class="col-75">
+      <textarea type="text" id="notes" name="notes" placeholder="Notes..."/>
+    </div>
+  </div>
+  <br/>
+  <div class="row">
+    <input type="submit" value="Submit"/>
+  </div>
+  </form>
+</div>
+  );
+}
+
+export default DiaryEntry;
